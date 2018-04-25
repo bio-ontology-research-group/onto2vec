23c271f1ed2b04df9c49d5ee43b2793f417fa3d6
Luke Slater <tinmachin3@gmail.com>
2018-04-25 15:04:48 +0100

    Fix getClasses.pl
    This was previously non-functional
    
    -Fix syntax errors
    -Fix file output
    -Add warnings/strict

--- getClasses.pl
+++ getClasses.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
-#use strict;
+use strict;
+use warnings;
+use autodie;
+
 #Define all Needed Variables
 use List::MoreUtils qw(uniq);
 my $path = `pwd`;
@@ -7,19 +10,20 @@ chomp ($path);
 my $inputfile= "$path/AllAxioms.lst";
 my $outputfile= "$path/AllClasses.lst";
 my @temparray=();
-open (FILE, '>>', "$addoutfile");
+open (FILE, '>>', "$outputfile");
 open (FH,"$inputfile");
 while (my $line =<FH>)
 {
-	chomp ($line);
-	if ($line =~ /(\S+)\s+/)
-		my $onto_class=$1;
-		push @temparray $onto_class;
+        chomp ($line);
+        if ($line =~ /(\S+)\s+/) {
+                my $onto_class=$1;
+                push @temparray, $onto_class;
+    }
 
 }
 my @Finalarray= uniq @temparray;
 foreach (@Finalarray)
 {
- 	my $final_class=$_;
-	print FILE "$final_class\n";
+        my $final_class=$_;
+        print FILE "$final_class\n";
 }
