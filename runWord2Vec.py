0f4c27b0ff6e1bab0f79d6915bd1a2094933e41d
Luke Slater <tinmachin3@gmail.com>
2018-04-25 15:08:54 +0100

    fix inconsistent tabs/spaces in indentation
      File "runWord2Vec.py", line 9
        for line in open('AllAxioms.lst'):
                                         ^
    TabError: inconsistent use of tabs and spaces in indentation

--- runWord2Vec.py
+++ runWord2Vec.py
@@ -6,7 +6,7 @@ class MySentences(object):
         self.dirname = dirname
 
     def __iter__(self):
-	for line in open('AllAxioms.lst'):
+    for line in open('AllAxioms.lst'):
                 yield line.split()
 
 sentences =gensim.models.word2vec.LineSentence('AllAxioms.lst') # a memory-friendly iterator
@@ -16,9 +16,9 @@ GOvectors={}
 word_vectors=ssmodel.wv
 file= open ('VecResults.lst', 'w')
 with open('AllClasses.lst') as f:
-	for line in f:
-		GO_class=line.rstrip()
-		if GO_class in word_vectors.vocab:
-			GOvectors[GO_class]=ssmodel[GO_class];
-			file.write (str(GO_class) + ' '+ str(GOvectors[GO_class]) +'\n')
+    for line in f:
+        GO_class=line.rstrip()
+        if GO_class in word_vectors.vocab:
+            GOvectors[GO_class]=ssmodel[GO_class];
+            file.write (str(GO_class) + ' '+ str(GOvectors[GO_class]) +'\n')
 file.close()
